f4eceb1b8a4ae8cadc8533fc6f775946f5e86cd5
Yadickson Soto <yadickson.soto@basedos.cl>
2017-11-08 15:00:41 -0300

    Add test support

---
+++ test/without/main.js
@@ -0,0 +1,38 @@
+var fontNpmFiles = require('../../');
+var path = require('path');
+
+require('should');
+
+describe('font-npm-files', function() {
+  function expect(filenames) {
+      var expectedFiles = [].concat(filenames).map(function(filename) {
+          return path.join(__dirname, filename);
+      });
+
+      function run(path, options, done) {
+
+          var srcFiles = fontNpmFiles();
+          srcFiles.should.be.eql(expectedFiles);
+
+          if (done) {
+              done();
+          }
+      }
+
+      return {
+          fromConfig: function(path, options) {
+              return {
+                  when: function(done) {
+                      run(path, options, done);
+                  }
+              };
+          }
+      };
+  }
+
+  it('should select the expected files with dependency', function(done) {
+      expect([])
+      .fromConfig()
+      .when(done);
+  });
+});
